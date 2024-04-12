92aa7378c29ec540171e50b83203a09bc930687a
Auzdora <1076388287@qq.com>
2024-04-12 16:59:27 +0800

    fix(p0): remove useless comments

--- src/byte_stream.cc
+++ src/byte_stream.cc
@@ -9,13 +9,11 @@ ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ), stream_() {
 
 bool Writer::is_closed() const
 {
-  // Your code here.
   return closed_;
 }
 
 void Writer::push( string data )
 {
-  // Your code here.
   if (is_closed()) {
     return;
   }
@@ -34,49 +32,41 @@ void Writer::push( string data )
 
 void Writer::close()
 {
-  // Your code here.
   closed_ = true;
 }
 
 uint64_t Writer::available_capacity() const
 {
-  // Your code here.
   return capacity_ - stream_.size();
 }
 
 uint64_t Writer::bytes_pushed() const
 {
-  // Your code here.
   return total_bytes_pushed_;
 }
 
 bool Reader::is_finished() const
 {
-  // Your code here.
   return closed_ && stream_.size() == 0;
 }
 
 uint64_t Reader::bytes_popped() const
 {
-  // Your code here.
   return total_bytes_poped_;
 }
 
 string_view Reader::peek() const
 {
-  // Your code here.
   return string_view(stream_);
 }
 
 void Reader::pop( uint64_t len )
 {
-  // Your code here.
   stream_.erase(0, len);
   total_bytes_poped_ += len;
 }
 
 uint64_t Reader::bytes_buffered() const
 {
-  // Your code here.
   return stream_.size();
 }
